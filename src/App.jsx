009c346178524a43c950bdb89b3d557bfd4e3a31
ValentinaPaladines <val.paladinezunal@gmail.com>
2022-12-28 16:50:33 -0500

    proyecto completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Cards from './assets/components/Cards'
 import cards from './assets/data/cards.jsx'
@@ -10,12 +10,19 @@ function App() {
     return index
   } 
 
-  const [index, setIndex] =useState(randomCard())
+  const [index, setIndex] =useState(randomCard(cards.length))
+
+  const colors = ["linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)", "linear-gradient(180deg, #FFFFFF 0%, #6284FF 50%, #FF0000 100%", "linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%", "linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%",
+  " linear-gradient(45deg, #FA8BFF 0%, #2BD2FF 52%, #2BFF88 90%)", "linear-gradient(0deg, #08AEEA 0%, #2AF598 100%", "linear-gradient(19deg, #21D4FD 0%, #B721FF 100%", "linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%", "linear-gradient(90deg, #74EBD5 0%, #9FACE6 100%", "linear-gradient(132deg, #F4D03F 0%, #16A085 100%" ];
 
   const changeIndex  = () => {
     const newRandom = Math.floor(Math.random( ) * cards.length)
     setIndex(newRandom)
   }
+
+  const colorChange = Math.floor(Math.random()* colors.length);
+  const color = colors[colorChange];
+  document.body.style=`background-image: ${color}`
 
   return (
     <div className="App">
@@ -25,7 +32,8 @@ function App() {
           <button onClick={changeIndex}>Aleatorio</button>
       </div>
     </div>
-  )
+  ) 
 }
 
 export default App
+
--- src/assets/components/Cards.jsx
+++ src/assets/components/Cards.jsx
@@ -1,17 +1,21 @@
 
 const Cards =({cardsData}) => {
 
-  const {img, name, number, propiedad, clave} = cardsData
+  const {img, name, number, propiedad, clave, } = cardsData
   
+  const colors = ['#6257D3', '#196F3D', '#BA4A00', '#7B241C', '#283747', '#6C3483', '#F1C40F', '#57D3CF', '#D35782', '#FF0400'];
+  const colorChange = Math.floor(Math.random()* colors.length);
+  const color = colors[colorChange];
+
   return (
     <div className="cards-container">
       <div className="card">
         <img src={`./src/assets/img/${img}`}></img>
         <div className="text">
-          <h3>{name}</h3>
-          <h4>{number}</h4>
-          <h4>{propiedad}</h4>
-          <h4 className="clave">{clave}</h4>
+          <h3 style={{color:color}}>{name}</h3>
+          <h4 style={{color:color}}>{number}</h4>
+          <h4 style={{color:color}}>{propiedad}</h4>
+          <h4 style={{color:color}} className="clave">{clave}</h4>
         </div>
       </div>
     </div>
