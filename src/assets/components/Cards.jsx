046a0c207ab9b7dfe0b7eed3972f6b08c1182a65
ValentinaPaladines <val.paladinezunal@gmail.com>
2023-01-03 16:23:42 -0500

    ruta modificacion

--- src/assets/components/Cards.jsx
+++ src/assets/components/Cards.jsx
@@ -6,11 +6,11 @@ const Cards =({cardsData}) => {
   const colors = ['#6257D3', '#196F3D', '#BA4A00', '#7B241C', '#283747', '#6C3483', '#F1C40F', '#57D3CF', '#D35782', '#FF0400'];
   const colorChange = Math.floor(Math.random()* colors.length);
   const color = colors[colorChange];
-
+  
   return (
     <div className="cards-container">
       <div className="card">
-        <img src={`./src/assets/img/${img}`}></img>
+        <img src={`${img}`}></img>
         <div className="text">
           <h3 style={{color:color}}>{name}</h3>
           <h4 style={{color:color}}>{number}</h4>
